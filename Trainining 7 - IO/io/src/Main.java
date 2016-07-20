3a27e5c3e69bd18e6fa1b127f183be3867a3dcf5
Ionut Ciuta <iciuta@endava.com>
2016-07-20 17:52:16 +0300

    ...

--- Trainining 7 - IO/io/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Character counter result: " + CharacterCounter.count("input", 'i'));
-        FileReverser.reverse("input");
-        FolderViewer.display("D:\\Trainings\\Trainining 7 - IO");
-    }
-}
